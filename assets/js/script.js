4b3d3d5758282ab35ceb0a8c26ca64cbe13c6fe5
NarminRahimova <nemorahimova11@gmail.com>
2024-01-29 09:52:21 +0400

    completed js-loops

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+//! Task-1 1-den 100-e qeder verilmis araliqda 3-e bolunen cut ededleri tapan kod yazin.
+// for(let i=1; i<100; i++){
+//     if(i%6==0){
+//         console.log(i);
+//     }
+// }
+
+//? Task-2 3 terefi verilmis bu fiqurun duzbucaqli olub-olmadigini tapan kod yazin.
+// let a=+prompt("a teref");
+// let b=+prompt("b teref");
+// let c=+prompt("c teref");
+// let d=+prompt("d teref");
+// if(a==c && b==d){
+//     console.log("Bu fiqur duzbucaqlidir");
+// }
+// else{
+//     console.log("Bu fiqur duzbucaqli deyil");
+// }
+
+//! Task-3 1-den 100-e qeder verilmis ededlerin icerisinde her iki reqemi eyni olan ededleri tapin.
+// for(let i=1; i<100; i++){
+//     if(i%11==0){
+//         console.log(i);
+//     }
+       
+// }
+
+//? Task-4  1-den 100-e qeder verilmis ededlerin icerisinde her iki reqemi muxtelif olan ededleri tapin.
+for(let i=1; i<100; i++){
+    if(i%11!=0 && i>=12 )
+    {
+     console.log(i);
+   }
+ }
+ 
+
